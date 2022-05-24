80c54c31eca8198526f1e8d93e7cbf85ede6c8f7
hannahhan153 <hannah.han@biola.edu>
2022-05-23 18:23:20 -0700

    still not functioning with small changes

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -77,7 +77,7 @@ const Signup = () => {
                 value={formState.password}
                 onChange={handleChange}
               />
-              <button className="btn d-block w-100" type="submit">
+              <button  className="btn d-block w-100" type="submit">
                 Submit
               </button>
             </form>
