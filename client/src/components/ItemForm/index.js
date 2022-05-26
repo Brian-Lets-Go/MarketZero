d862d9964dfec2865e0c7de297463e1f23df694b
hannahhan153 <hannah.han@biola.edu>
2022-05-25 19:30:49 -0700

    style changes

--- client/src/components/ItemForm/index.js
+++ client/src/components/ItemForm/index.js
@@ -72,21 +72,21 @@ const ItemForm = () => {
             {error && <span className="ml-2">Something went wrong...</span>}
           </p>
           <form className="flex-row justify-center justify-space-between-md align-stretch"  onSubmit={handleFormSubmit}>
-          <label htmlFor="item-name">Name</label>
-      <input className="btn col-12 col-md-8"  type="text" placeholder="Item Name" id="item-name" name="item-name" onBlur={(e) => {
+          <label className= " col-12 col-md-8" htmlFor="item-name"></label>
+      <input className="btn col-12 col-md-8 "  type="text" placeholder="Item Name" id="item-name" name="item-name" onBlur={(e) => {
           console.log(name)
           setName(e.target.value)
           console.log(name)
           }}></input>
-      <label htmlFor="description">Description</label>
+      <label htmlFor="description"></label>
       <textarea className="btn col-12 col-md-8" type="text" placeholder="Item Description" value={description} onChange={handleChange}id="description" name="description"  ></textarea>
-      <label htmlFor="quantity">Price</label>
+      <label htmlFor="quantity"></label>
     <input className="btn col-12 col-md-8" placeholder="Item Price" type="number" id="quantity" name="quantity" onBlur={(e) => {
         console.log(price)
         setPrice(parseFloat(e.target.value))
         console.log(price)
     }}></input>
-            <label htmlFor="category-names">Choose a category: </label>
+            <label htmlFor="category-names"></label>
             <select className="btn col-12 col-md-8" name="category-names" id="category-names" onChange={(e) => {
               setCategory(e.target.value)
               console.log(category)
@@ -97,7 +97,7 @@ const ItemForm = () => {
                 <option value="Receptacles">Receptacles</option>
                 <option value="Miscellaneous">Miscellaneous</option>
             </select>
-            <label htmlFor="condition-names">Condition Its Condition Is In: </label>
+            <label htmlFor="condition-names"></label>
             <select className="btn col-12 col-md-8" name="condition-names" id="condition-names" onChange={(e) => {
               setCondition(e.target.value)
               console.log(condition_its_condition_is_in)
