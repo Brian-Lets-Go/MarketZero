713605b4f17292f9c7aae8294dda34ab797638c8
jAmWhisk <merrychu1005@gmail.com>
2022-05-26 20:11:20 -0400

    category section

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,32 +10,88 @@ import '../index.css';
 import "../fonts/BancoRegular.woff";
 
 const Home = () => {
-    const { loading, data } = useQuery(QUERY_ITEMS);
-    // const { data: userData } = useQuery(QUERY_ME_BASIC);
+  const { loading, data } = useQuery(QUERY_ITEMS);
+  // const { data: userData } = useQuery(QUERY_ME_BASIC);
 
-    const items = data?.items || [];
-    console.log(items);
+  const items = data?.items || [];
+  console.log(items);
 
-    const loggedIn = Auth.loggedIn();
+  const loggedIn = Auth.loggedIn();
 
-    return (
-        <main>
-          <div className='flex-row justify-space-between'>
-          {loggedIn && (
-        <div className="col-12 mb-3">
-            {/* <ItemForm /> */}
+  return (
+    <main>
+      <section id="items">
+        <div className='section-bg'>
+          <div className='category-section'>
+            <div className='gif-section'>
+              <img className='gif' src={all} alt="" />
+              <h3 className='overlay'>All</h3>
+            </div>
+          </div>
         </div>
+
+        <div className='section-bg'>
+          <div className='category-section'>
+            <div className='gif-section'>
+              <img className='gif' src={bowling} alt="" />
+              <h3 className='overlay'>Bowling</h3>
+            </div>
+          </div>
+        </div>
+
+        <div className='section-bg'>
+          <div className='category-section'>
+            <div className='gif-section'>
+              <img className='gif' src={alcohol} alt="" />
+              <h3 className='overlay'>Alcohol</h3>
+            </div>
+          </div>
+        </div>
+
+        <div className='section-bg'>
+          <div className='category-section'>
+            <div className='gif-section'>
+              <img className='gif' src={rug} alt="" />
+              <h3 className='overlay'>Rugs</h3>
+            </div>
+          </div>
+        </div>
+
+        <div className='section-bg'>
+          <div className='category-section'>
+            <div className='gif-section'>
+              <img className='gif' src={bereavement} alt="" />
+              <h3 className='overlay'>Bereavement Receptacles</h3>
+            </div>
+          </div>
+        </div>
+
+        <div className='section-bg'>
+          <div className='category-section'>
+            <div className='gif-section'>
+              <img className='gif' src={other} alt="" />
+              <h3 className='overlay'>Others/ Miscellaneous</h3>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className='flex-row justify-space-between'>
+        {loggedIn && (
+          <div className="col-12 mb-3">
+            {/* <ItemForm /> */}
+          </div>
         )}
-          <div className={`col-12 mb-3 ${loggedIn &&'col-lg-8'}`}>
-      {loading ? (
-        <div>Loading...</div>
+        <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
+          {loading ? (
+            <div>Loading...</div>
           ) : (
             <ItemList items={items} title="Some Item(s)..." />
           )}
-          </div>
         </div>
-      </main>
-    );
+      </div>
+    </main>
+  );
 };
 
 export default Home;
